917a32510e154346d5847bbdb5c4a2f0c78f9936
Lassi Piironen <lassi.piironen@kone.com>
2025-07-25 15:36:47 +0300

    Add a catch-all feed sourcer, add Tivi rss feed

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 news_sources = [
-    "https://newsapi.org/v2/top-headlines?sources=reuters,bbc-news,cnn",
+    #"https://newsapi.org/v2/top-headlines?sources=reuters,bbc-news,cnn",
     "https://www.hs.fi/rss/tuoreimmat.xml",
     "https://www.is.fi/rss/tuoreimmat.xml",
     "https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET",
     "https://feeds.kauppalehti.fi/rss/main",
+    "https://www.tivi.fi/api/feed/v2/rss/tv",
 ]
 
 """
